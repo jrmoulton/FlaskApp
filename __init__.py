232342b912339a824aa8a0f7bc252be97510eba1
Jared Moulton <jaredmoulton3@gmail.com>
2021-02-09 13:01:18 -0700

    Add functional stuff

--- __init__.py
+++ __init__.py
@@ -3,8 +3,12 @@ from flask import request
 import requests
 from bs4 import BeautifulSoup as bs
 
+app = Flask(__name__)
 
-def parse(r, id: str) -> str:
+logged_in = False
+
+
+def parse(r, id):
     text = r.text
     soup = bs(text, "html.parser")
     line = soup.find(id=id)
@@ -12,7 +16,7 @@ def parse(r, id: str) -> str:
     return line.get("value")
 
 
-def login(username: str, password: str) -> int:
+def login(username, password, s):
     # Get the initial login page with the sync token
     r_one = s.get('https://aggietime.usu.edu/login/auth')
     print(r_one.status_code)
@@ -24,7 +28,9 @@ def login(username: str, password: str) -> int:
     sync_uri = parse(r_one, 'SYNCHRONIZER_URI')
 
     # Sign in to the initial login page
-    headers_one = {
+    length = str(len(
+        f"SYNCHRONIZER_TOKEN={sync_token}&SYNCHRONIZER_URI={sync_uri}&j_username={username}&j_password={password}&login-submit="))
+    login_page_headers = {
         'Host': 'aggietime.usu.edu',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Origin': 'https://aggietime.usu.edu',
@@ -33,48 +39,47 @@ def login(username: str, password: str) -> int:
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15',
         'Referer': 'https://aggietime.usu.edu/login/auth',
-        'Content-Length': '150',
+        'Content-Length': length,
         'Accept-Language': 'en-us',
     }
 
     r_two = s.post("https://aggietime.usu.edu/j_spring_security_check",
-                   data=f"SYNCHRONIZER_TOKEN={sync_token}&SYNCHRONIZER_URI={sync_uri}&j_username={username}&j_password={password}&login-submit=", headers=headers_one)
+                   data=f"SYNCHRONIZER_TOKEN={sync_token}&SYNCHRONIZER_URI={sync_uri}&j_username={username}&j_password={password}&login-submit=", headers=login_page_headers)
     if r_two.status_code == 200:
-        print('Succesful login!\n')
+        return 'Succesful login!\n'
     else:
-        print("failure at login\n")
-    return r_two.status_code
+        return "Failure logging in\n"
 
 
-def get_dashboard():
+def get_dashboard(username, password, s):
+
+    if not logged_in:
+        login(username, password, s)
+
     # Get the dashboard page that has the next sync_token
-    data = {}
+    variables = {}
+    print(s.cookies)
     r = s.get("https://aggietime.usu.edu/dashboard")
     print(r.status_code)
     if r.status_code == 200:
         print('Success at getting dashboard!')
     else:
         print("failure getting dashboard")
-    data['sync_token'] = parse(r, 'SYNCHRONIZER_TOKEN')
-    data['sync_uri'] = parse(r, 'SYNCHRONIZER_URI')
-    data['posId'] = parse(r, 'posId')
-    data['toStatus'] = parse(r, 'toStatus')
-    return data
+    variables['sync_token'] = parse(r, 'SYNCHRONIZER_TOKEN')
+    variables['sync_uri'] = parse(r, 'SYNCHRONIZER_URI')
+    variables['toStatus'] = parse(r, 'toStatus')
+    variables['posId'] = parse(r, 'posId')
+
+    return variables
 
 
-def clock_status():
-    data = get_dashboard()
-    if data['toStatus'] == "OUT":
-        return "You are clocked in"
-    else:
-        return "You are clocked out"
+def punch_clock(username, password, inout, s):
 
+    variables = get_dashboard(username, password, s)
 
-def clock(inout: str):
-
-    data = get_dashboard()
-
-    headers_two = {
+    length = str(len(
+        f"SYNCHRONIZER_TOKEN={variables['sync_token']}&SYNCHRONIZER_URI={variables['sync_uri']}&posId={variables['posId']}&comment=&projectName=&toStatus={inout}"))
+    clock_punch_headers = {
         'Host': 'aggietime.usu.edu',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Origin': 'https://aggietime.usu.edu',
@@ -83,49 +88,55 @@ def clock(inout: str):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15',
         'Referer': 'https://aggietime.usu.edu/dashboard',
-        'Content-Length': '189',
+        'Content-Length': length,
         'Accept-Language': 'en-us',
     }
     r = s.post("https://aggietime.usu.edu/dashboard/clock/punch",
-               data=f"SYNCHRONIZER_TOKEN={data['sync_token']}&SYNCHRONIZER_URI={data['sync_uri']}&posId={data['posId']}&comment=&projectName=&toStatus={inout}", headers=headers_two)
+               data=f"SYNCHRONIZER_TOKEN={variables['sync_token']}&SYNCHRONIZER_URI={variables['sync_uri']}&posId={variables['posId']}&comment=&projectName=&toStatus={inout}", headers=clock_punch_headers)
     if r.status_code == 200:
-        print('Success punching clock!')
+        if variables['toStatus'] == 'OUT':
+            return 'Success clocking out'
+        else:
+            return 'Success clocking in'
     elif r.status_code == 500:
-        print("Error 500. You probably clocked out when you need to clock in")
+        return "Error 500. You probably clocked out when you need to clock in"
     else:
-        print("Failure punching clock")
-    return r.status_code
+        return "Failure punching clock"
 
 
-app = Flask(__name__)
+def clock_status(username, password, s):
+    variables = get_dashboard(username, password, s)
+    if variables['toStatus'] == "OUT":
+        return "You are clocked in"
+    else:
+        return "You are clocked out"
 
 
-@app.route('/aggietime/status/', methods=['GET', 'POST'])
-def status():
-    if request.method == 'POST':
-        username: str = str(request.form.get('username'))
-        print(username)
-        password: str = str(request.form.get('password'))
-        login(username, password)
-        status = clock_status()
-        return status
-    return "Please send a form post request"
-
-
-@app.route('/aggietime', methods=['GET', 'POST'])
+@app.route('/aggietime/', methods=['GET', 'POST'])
 def main():
+    s = requests.Session()
     if request.method == 'POST':
         username: str = str(request.form.get('username'))
         password: str = str(request.form.get('password'))
         inout: str = str(request.form.get('inout'))
-        login(username, password)
-        r = clock(inout)
-        return str(r)
+        r = punch_clock(username, password, inout, s)
+        return r
     return "Please send a form POST request with your login information"
 
 
-if __name__ == "__main__":
+@app.route('/aggietime/status/', methods=['GET', 'POST'])
+def status():
     s = requests.Session()
-    context = ('/etc/letsencrypt/live/jrmoulton.com-0001/fullchain.pem',
-               '/etc/letsencrypt/live/jrmoulton.com-0001/privkey.pem')
-    app.run()
+    if request.method == 'POST':
+        username: str = str(request.form.get('username'))
+        password: str = str(request.form.get('password'))
+        status_code = clock_status(username, password, s)
+        return status_code
+    return "Please send a form post request"
+
+
+if __name__ == "__main__":
+    context = ('/etc/letsencrypt/live/jrmoulton.com/fullchain.pem',
+               '/etc/letsencrypt/live/jrmoulton.com//privkey.pem')
+    app.run(debug=True, ssl_context=context, host='jrmoulton.com')
+    # app.run()
